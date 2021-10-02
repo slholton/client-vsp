ddf148260a7e2d6a6602e8a57e6db8a5842d7b57
Shavonne Holton <shavonne.holton@gmail.com>
2021-10-02 12:06:00 -0700

    updated APIURL

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'https://my-vsp-client.herokuapp.com/':
+    case 'https://my-vsp-client.herokuapp.com':
         APIURL = 'https://my-vsp-db.herokuapp.com';
         break;
     default:
